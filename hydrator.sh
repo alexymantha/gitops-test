1a1d01566bc742005d66428a944c961f07953e39
Alexy Mantha <alexy@mantha.dev>
2025-03-07 15:28:27 -0500

    test manual hydration
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

---
+++ hydrator.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git branch -D environment/development-next
+git checkout -B environment/development-next
+
+sed -i 's/%ENVIRONMENT%/dev/g' file.yaml
+jq -cn --arg sha $(git rev-parse HEAD) '{drySha: $sha}' > hydrator.metadata
+git add file.yaml hydrator.metadata
+git commit -m "Hydrate from main" && git push --force
+
+
+git checkout main
