98ec414e7efab0f3c53db2cbbe561e3f866a6ed9
Alexy Mantha <alexy@mantha.dev>
2025-03-07 15:41:18 -0500

    test
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- hydrator.sh
+++ hydrator.sh
@@ -6,6 +6,6 @@ git checkout -B environment/development-next
 sed -i 's/%ENVIRONMENT%/dev/g' file.yaml
 jq -cn --arg sha $(git rev-parse HEAD) '{drySha: $sha}' > hydrator.metadata
 git add file.yaml hydrator.metadata
-git commit -m "Hydrate from main" && git push --force origin
+git commit -m "Hydrate from main" && git push --force origin environment/development-next
 
 git checkout main
